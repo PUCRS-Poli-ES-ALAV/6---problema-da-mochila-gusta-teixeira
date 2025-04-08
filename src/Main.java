11a860a88872272033a7df2be04d3bc7dc5f834a
gustateixeira <gustavohteixeira2904@gmail.com>
2025-04-07 21:49:44 -0300

    feito

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,78 @@
-import java.util.Arrays;
 
 public class Main{
+    public static int iter = 0;
+    public static int instru = 0;
+
     public static void main(String[] args) {
-        int [][] itens = {{0,0},{5, 2}, {2,2}, {2,4}, {1,3}}; //i = peso,  j = valor
-        System.out.println(backPackPd(itens.length, 9, itens));
+        int[] capacidades = {9, 165, 190};
+        int[][][] valores = {{{0,0},{5, 2}, {2,2}, {2,4}, {1,3}}
+        ,{
+            {0, 0},
+            {56, 50},
+            {59, 50},
+            {80, 64},
+            {64, 46},
+            {75, 50},
+            {17, 5}
+        },
+        {
+            {0, 0},
+            {23, 92},
+            {31, 57},
+            {29, 49},
+            {44, 68},
+            {53, 60},
+            {38, 43},
+            {63, 67},
+            {85, 84},
+            {89, 87},
+            {82, 72}
+        }};
+
+        
+        System.out.printf("%5s | %10s | %10s | %12s | %15s\n", "n", "Resultado", "Iterações", "Instruções", "Tempo (s)");
+        int j = 0;
+        
+        System.out.println("----------------------------------------------------------------------");
+
+        for(int [][] itens : valores){
+            iter = 0;
+            instru = 0;
+            long inicio = System.nanoTime();
+            long result = backPackPd(itens.length, capacidades[j], itens);
+            long fim = System.nanoTime();
+            long tempo = fim - inicio;
+            System.out.printf("%5d | %10d | %10d | %12d | %.12f\n", capacidades[j], result, iter, instru, tempo*1e-9);
+            j++;
+
+        }
+
     }
 
     public static int backPackPd(int n, int c, int[][] itens){
         final int N = n;
+        instru++;
         final int C = c;
-        int iter = 1;
+        instru++;
+        iter++;
         int [][] maxTab = new int[N+1][C+1]; 
+        instru++;
 
         for(int i = 1; i < N; i++){
             for(int j = 1; j < C; j++){
                 iter++;
                 if(itens[i][0] <= j){
+                    instru++;
                     maxTab[i][j] = Math.max(maxTab[i-1][j], itens[i][1] + maxTab[i-1][j-itens[i][0]]);
                 }else{
+                    instru++;
                     maxTab[i][j] = maxTab[i-1][j];
                 }
                 }
             }
 
-        for(int[] i : maxTab){
-            System.out.println(Arrays.toString(i));
-        }
-        System.out.println("iteracoes: " + iter);
+        
         return maxTab[N-1][C-1];
         }
+
 }
--- src/main.py
+++ src/main.py
@@ -1,35 +1,82 @@
+import time
+iteracoes = 0
+instrucoes = 0
+
+
 def fibo_rec(n):
+    global instrucoes, iteracoes
+    iteracoes +=1
     if n <= 1:
+        instrucoes += 2
         return n
+    instrucoes +=1
     return fibo_rec(n-1) + fibo_rec(n - 2)
 
 
 def fibo(n):
+    global iteracoes, instrucoes
+    iteracoes +=1
     f = []
-    f[0] = 0
-    f[1] = 1
+    instrucoes +=1
+    f.append(0)
+    instrucoes +=1
+    f.append(1)
+    instrucoes +=1
 
-    for i in range(2, n):
-        f[i] = f[i-1] + f[i-2]
-    
-    return f[n]
+    for i in range(2, n+1):
+        f.append(f[i-1] + f[i-2])
+        iteracoes +=1
+        instrucoes +=1
+    instrucoes +=1
+
+    return f.pop()
 
 def memoized_fibo(n):
+    global iteracoes, instrucoes
+    iteracoes += 1
     f = []
-    for i in range(0,n):
-        f[i] = -1
+    instrucoes +=1
+    for i in range(0,n+1):
+        f.append(-1)
+        iteracoes +=1
+    instrucoes +=1
     return lookup_fibo(f, n)
 
 
 def lookup_fibo(f,n):
+    global iteracoes, instrucoes
+    iteracoes +=1
     if f[n] >= 0:
+        instrucoes +=1
         return f[n]
     elif n <= 1:
+        instrucoes +=1
         f[n] = n
     else:
+        instrucoes +=1 
         f[n] = lookup_fibo(f, n-1) + lookup_fibo(f, n-2)
+    instrucoes +=1     
     return f[n]
 
 
 
+
+
+print(f"{'n':>5} | {'Resultado':>9} | {'Iterações':>10} | {'Instruções':>12} | {'Tempo (s)':>10}")
+print("-" * 60)
+
+
+valores = [4,8,16,32, 128, 1000, 10000]
+
+for n in valores:
+    iteracoes = 0
+    instrucoes = 0
+
+    inicio = time.perf_counter_ns()
+    resultado = memoized_fibo(n)
+    fim = time.perf_counter_ns()
+
+    tempo = fim - inicio
+
+    print(f"{n:>5} | {resultado:>9} | {iteracoes:>10} | {instrucoes:>12} | {tempo*1e-9:>10.12f}")
 
